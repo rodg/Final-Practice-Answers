b8c8fad32cc00ba62e27d60beca578312c9d7109
Rod Gangnath <41761494+rodg@users.noreply.github.com>
2018-12-12 17:12:59 -0500

    Package visibility answers fixed

--- Code/threads/DataRace.java
+++ Code/threads/DataRace.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class DataRace {
 
 	public static void main(String[] args) {
+		System.out.println("Im run");
 		
 		ArrayList<String> bananas = new ArrayList<String>();
 		for(int i = 0; i <= 20; i++)
@@ -20,14 +21,7 @@ public class DataRace {
 			for(int i = 0; i <=20; i+=2)
 				bananas.add(i, "orange");
 		};
-		
-		Thread test = new Thread(()->{
-			for(String b: bananas) {
-				System.out.println(b);
-			}
-		});
-		
-		
+				
 		Thread reader = new Thread(A);
 		Thread writer = new Thread(B);
 		reader.start();
